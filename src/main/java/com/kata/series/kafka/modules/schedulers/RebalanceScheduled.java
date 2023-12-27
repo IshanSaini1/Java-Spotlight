7499d4e076ade9868b94a09d8846b08666598b4e
Ishan Saini <isaini331@gmail.com>
2023-12-27 18:26:43 +0530

    commenting schedulers

--- src/main/java/com/kata/series/kafka/modules/schedulers/RebalanceScheduled.java
+++ src/main/java/com/kata/series/kafka/modules/schedulers/RebalanceScheduled.java
@@ -1,9 +1,6 @@
 package com.kata.series.kafka.modules.schedulers;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.kata.series.kafka.modules.producers.RebalanceProducer;
@@ -14,7 +11,7 @@ public class RebalanceScheduled {
 	@Autowired
 	private RebalanceProducer producer;
 
-	@Scheduled(fixedRate = 2, timeUnit = TimeUnit.SECONDS)
+	//@Scheduled(fixedRate = 2, timeUnit = TimeUnit.SECONDS)
 	public void triggerProducer() {
 		producer.sendMessage();
 	}
